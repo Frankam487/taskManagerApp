dcc05062a2b326227c72f9b949c76e3d5666a8fa
Frank kamagang <fkamgang06@gmail.com>
2024-08-11 22:25:19 +0200

    edit feature added

--- src/components/CardTask.jsx
+++ src/components/CardTask.jsx
@@ -2,28 +2,46 @@ import axios from "axios";
 import { useState } from "react";
 
 const CardTask = ({ task }) => {
+    const [edit, setEdit] = useState(false);
+    const [titleEdit, setTitleEdit] = useState("");
+    const [textareaEdit, setTextareaEdit] = useState("");
     const handleDelete = (id) => {
-         axios.delete(`http://localhost:1000/tasks/${id}`)
+        axios.delete(`http://localhost:1000/tasks/${id}`)
         window.location.reload();
     }
-
+    const handleEdit = (id) => {
+        const data = {
+            title: titleEdit ? titleEdit : task.title,
+            description: textareaEdit ? textareaEdit : task.description
+        }
+        axios.put("http://localhost:1000/tasks/" + id, data).then(() => {
+            setEdit(false);
+        });
+        // window.location.reload();
+    }
     return (
 
         <div className="card">
-            <h2>{task.title}</h2>
-            <p>{task.description}</p>
+
+            {edit ? <input type="text" defaultValue={titleEdit ? titleEdit : task.title} onChange={(e) => setTitleEdit(e.target.value)} /> : <h2>{titleEdit ? titleEdit : task.title}</h2>}
+            {edit ? <textarea defaultValue={textareaEdit ? textareaEdit : task.description} onChange={(e) => setTextareaEdit(e.target.value)}></textarea> : <p>{textareaEdit ? textareaEdit : task.description}</p>}
+
             <div className="btn-content">
-            <select >
-                <option value="">No status filter</option>
-                <option value="">Open</option>
-                <option value="">Progress</option>
-            </select>
-            <button onClick={() => {
-                if (window.confirm("Supprimer ?")) {
-                    handleDelete(task.id)
-                }
-            }
-            }>Delete</button>
+                <select >
+                    <option value="">No status filter</option>
+                    <option value="">Open</option>
+                    <option value="">Progress</option>
+                </select>
+                <div className="btn">
+                    {edit ? <button onClick={() => handleEdit(task.id)} className="edit">Valider</button> : <button onClick={() => setEdit(true)} className="edit"
+                    >Edit</button>}
+                    <button className="delete" onClick={() => {
+                        if (window.confirm("Supprimer ?")) {
+                            handleDelete(task.id)
+                        }
+                    }
+                    }>Delete</button>
+                </div>
             </div>
         </div>
     );
